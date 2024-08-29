0140d4ce42de1c471cddc90d399253ddd0c04f91
theryanfo <ryan.s.fordham@gmail.com>
2024-08-28 22:25:19 -0700

    GET and POST mostly complete

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ def create_contact():
     email = request.json.get("email")
 
     if not first_name or not last_name or not email:
-        return (jsonify({"message": "You must include a first name, last name and email"}), 400,)
+        return (jsonify({"message": "You must include a first name, last name, and email"}), 400,)
     
     new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
     try:
@@ -26,12 +26,33 @@ def create_contact():
     
     return (jsonify({"message": "User added!"}), 201)
 
-@app.route("/update_contact/<int:user_id", methods=["PATCH"])
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
 def update_contact(user_id):
     contact = Contact.query.get(user_id)
 
     if not contact:
-        return jsonify({"message": "User not found"})
+        return jsonify({"message": "User not found"}), 404
+    
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "User updated!"}), 200
+
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
+def delete_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    
+    db.session.delete(contact)
+    db.session.commit()
+
+    return jsonify({"message": "User deleted!"}), 200
 
 
 if __name__ == "__main__":
---
+++ frontend/src/ContactList.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+const ContactList = ({contacts}) => {
+    return (<div>
+        <h2>Contacts</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Emails</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {contacts.map((contact) => (
+                    <tr key={contact.id}>
+                        <td>{contact.firstName}</td>
+                        <td>{contact.lastName}</td>
+                        <td>{contact.email}</td>
+                        <td>
+                            <button>Update</button>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>)
+}
+
+export default ContactList
